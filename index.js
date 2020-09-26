c38697ffbd5055ae19e7c34f434a24c8777020e4
Thomas Gale <thomasgale@Thomas-Mac-Mini.broadband>
2020-09-26 18:38:07 +0100

    commit on master

--- index.js
+++ index.js
@@ -3,8 +3,58 @@ let difficulty = "easy";
 let stage = "read";
 let boxes = 36;
 let maxInt = 4;
-let time = 8;
+let time = 6;
 let status;
+let score;
+let rounds;
+let inGame = false;
+function l(text){
+  console.log(text)
+}
+function updateScore(){
+  if(status=="win"){
+    document.getElementById("scoreTxt").innerHTML = score+"/"+rounds*36;
+    startGame();
+  }else{
+    document.getElementById("scoreTxt").innerHTML = "Previous Score: " +score+"/"+rounds*36;
+    score = 0;
+    rounds = 0;
+  };
+}
+function resetBoard(color, gen, sel){
+    for (let i = 0; i < boxes; i++) {
+      if(color==true){
+        document.getElementById('box'+i).style.backgroundColor = 'white';
+      };
+      if(gen==true){
+        document.getElementById('box'+i).setAttribute("data-generated", "0");
+      };
+      if(sel==true){
+        document.getElementById('box'+i).setAttribute("data-selected", "0");
+      }
+    };
+}
+function validateBoard(){
+  for (let i = 0; i < boxes; i++) {
+    if(document.getElementById('box'+i).getAttribute("data-generated") == document.getElementById('box'+i).getAttribute("data-selected")){
+      document.getElementById('box'+i).style.backgroundColor = 'green';
+      score += 1;
+    }else{
+      document.getElementById('box'+i).style.backgroundColor = 'red';
+      status = "lose";
+    };
+  };
+}
+function randomiseBoard(){
+  for (let i = 0; i < boxes; i++) {
+    if(randint(0, maxInt)==0) {
+      //setting random board
+      document.getElementById('box'+i).style.backgroundColor = 'gray';
+      document.getElementById('box'+i).setAttribute("data-generated", "1");
+    };
+  };
+  console.log('randomised board')
+}
 function randint(min, max){
   return(Math.floor(Math.random() * (max-min))+min)
 }
@@ -21,83 +71,69 @@ function changebox(boxNum){
   }
 }
 function setMode(mode){
-  status = "win";
-  if(mode=="easy"){
-    difficulty = "easy";
-    maxInt = 4;
-    time = 8;
-    document.getElementById("timerBar").style.animationDuration = time+"s";
-    document.getElementById("hardButton").style.backgroundColor = 'white';
-    document.getElementById("easyButton").style.backgroundColor = 'gray';
-  } else if(mode=="hard") {
-    difficulty = "hard";
-    maxInt = 2;
-    time = 8;
-    document.getElementById("timerBar").style.animationDuration = time+"s";
-    document.getElementById("hardButton").style.backgroundColor = 'gray';
-    document.getElementById("easyButton").style.backgroundColor = 'white';
+    if(inGame==false){
+    status = "win";
+    if(mode=="easy"){
+      difficulty = "easy";
+      maxInt = 4;
+      time = 6;
+      document.getElementById("timerBar").style.animationDuration = time+"s";
+      document.getElementById("hardButton").style.backgroundColor = 'white';
+      document.getElementById("mediumButton").style.backgroundColor = 'white';
+      document.getElementById("easyButton").style.backgroundColor = 'gray';
+    } else if(mode=="medium") {
+      difficulty = "medium";
+      maxInt = 2;
+      time = 7;
+      document.getElementById("timerBar").style.animationDuration = time+"s";
+      document.getElementById("hardButton").style.backgroundColor = 'white';
+      document.getElementById("mediumButton").style.backgroundColor = 'gray';
+      document.getElementById("easyButton").style.backgroundColor = 'white';
+    } else if(mode=="hard") {
+      difficulty = "hard";
+      maxInt = 2;
+      time = 8;
+      document.getElementById("timerBar").style.animationDuration = time+"s";
+      document.getElementById("hardButton").style.backgroundColor = 'gray';
+      document.getElementById("mediumButton").style.backgroundColor = 'white';
+      document.getElementById("easyButton").style.backgroundColor = 'white';
+    }
+  }else{
   }
 }
 function startGame(){
-  let score = 0;
-  let rounds = 0;
+  // user memorising
+  inGame = true;
+  score = 0;
+  rounds = 0;
   stage = "read";
-  for (let i = 0; i < boxes; i++) {
-    document.getElementById('box'+i).style.backgroundColor = 'white';
-    //resetting board
-    document.getElementById('box'+i).setAttribute("data-generated", "0");
-    if(randint(0, maxInt)==0) {
-      //setting random board
-      document.getElementById('box'+i).style.backgroundColor = 'gray';
-      document.getElementById('box'+i).setAttribute("data-generated", "1");
-    };
-  };
+  randomiseBoard();
   document.getElementById("startButton").style.visibility = "hidden";
   document.getElementById("timerBar").classList.add('barTimerTrigger');
-  setTimeout(()=>{
-    console.log("1");
+  setTimeout(()=>{ // prompting users guess
+    l(1);
     document.getElementById("timerBar").classList.remove('barTimerTrigger');
-    for (let i = 0; i < boxes; i++) {
-      document.getElementById('box'+i).style.backgroundColor = 'white';
-    }; 
-    console.log("2");
+    resetBoard(true, false, false)
     stage = "write";
     document.getElementById("timerBar").classList.remove('barTimerTrigger');
     setTimeout(() => {
+      l(2);
       //in a timeout cus doesnt work when next to its previous removal
       //idk how to add then remove a class any other way
       document.getElementById("timerBar").classList.add('barTimerTrigger')
     }, 20)
-    setTimeout(()=>{
-      console.log("3");
+    setTimeout(()=>{ //review game
+      l(3);
       document.getElementById("timerBar").classList.remove('barTimerTrigger');
       stage = "read";
       rounds += 1;
-      for (let i = 0; i < boxes; i++) {
-        if(document.getElementById('box'+i).getAttribute("data-generated") == document.getElementById('box'+i).getAttribute("data-selected")){
-          document.getElementById('box'+i).style.backgroundColor = 'green';
-          score += 1;
-        }else{
-          document.getElementById('box'+i).style.backgroundColor = 'red';
-          status = "lose";
-        };
-      };
+      validateBoard();
       setTimeout(()=>{
-        console.log("4");
-        for (let i = 0; i < boxes; i++) {
-          document.getElementById('box'+i).style.backgroundColor = 'white';
-          document.getElementById('box'+i).setAttribute("data-generated", "0");
-          document.getElementById('box'+i).setAttribute("data-selected", "0");
-        }
+        l(4);
+        resetBoard(true, true, true);
         document.getElementById("startButton").style.visibility = "visible";
-        if(status=="win"){
-          document.getElementById("scoreTxt").innerHTML = score+"/"+rounds*36;
-          startGame();
-        }else{
-          document.getElementById("scoreTxt").innerHTML = "Previous Score: " +score+"/"+rounds*36;
-          score = 0;
-          rounds = 0;
-        };
+        updateScore();
+        inGame = false
       }, 1250)
     },time*1000+20);
   }, time*1000);
